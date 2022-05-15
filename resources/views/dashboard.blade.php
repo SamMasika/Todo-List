093d44fa39575e388fb4d7c1ee31f8652b32d249
SamMasika <masikasamwel6@gmail.com>
2022-05-15 08:31:25 +0300

    db

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,10 +24,21 @@
               
               
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                  <div class="container-fluid">
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                      <div class="navbar-nav">
-                        <a class="nav-link" href="index">Create Task</a>
+                    <div class="container-fluid">
+                      <a class="navbar-brand" href="#">TodoList</a>
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                          <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index">Home</a>
+                          </li>
+                        
+                        </ul>
+                      </div>
+                    </div>
+                  </nav>
                         
                     </div>
                   </div>
