75c2742798c179f5e1c389fb91d53c6fcb36a3ae
SamMasika <masikasamwel6@gmail.com>
2022-05-15 08:35:41 +0300

    db

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,7 +32,7 @@
                       <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav">
                           <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index">Home</a>
+                            <a class="nav-link active" aria-current="page" href="index">Create Task</a>
                           </li>
                         
                         </ul>
